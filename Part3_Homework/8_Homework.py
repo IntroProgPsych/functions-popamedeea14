4720d1d006af28809231e2e4d00e6bb739ad9001
popamedeea14 <maria.popa06@e-uvt.ro>
2025-11-19 20:43:49 +0000

    Completed 8_Homework

--- Part3_Homework/8_Homework.py
+++ Part3_Homework/8_Homework.py
@@ -19,3 +19,25 @@
 # Keep input() outside the functions.
 
 # Write your code here:
+
+def calculate_grade(score):
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"
+      
+def display_report(score, grade):
+    print(f"Score: {score}")
+    print(f"Grade: {grade}")
+
+
+
+score = int(input("Enter the score: "))
+grade = calculate_grade(score)
+display_report(score, grade) 
